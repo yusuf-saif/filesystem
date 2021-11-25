26f9ccbb8b780a73538313dc80ce5fe93dffc4d1
yusuf_saif <ysaifullah28kbg@gmail.com>
2021-11-25 12:17:57 +0100

    fixed bugs and added new changes

--- fms_db.sql
+++ fms_db.sql
@@ -1,30 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 19, 2021 at 06:20 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
 -- Database: `fms_db`
---
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `files`
---
 
 CREATE TABLE `files` (
   `id` int(11) NOT NULL,
@@ -129,6 +113,3 @@ ALTER TABLE `folders`
 --
 ALTER TABLE `users`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
